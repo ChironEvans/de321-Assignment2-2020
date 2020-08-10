216c8df9c0698a9d68cd9ab7cda3792bf4a344ab
ChironEvans <chiron.evans@gmail.com>
2020-08-11 11:38:33 +1200

    Regex Parsing started
    Using regex to fetch data from js file, can fetch objects, attributes so far. Handles multiple objects in one file.

--- test-nosejs.py
+++ test-nosejs.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import dukpy
 import js2py
+import re
 
 def run_nosejs(path):
     # command args that disable the unittest portion of the conversion are broken...bleh
@@ -29,10 +30,44 @@ def run_js2py(file):
             js_input += line
     print(js2py.eval_js6(js_input))
 
+def run_regex(file):
+    js_input = ''
+    with open(f'js_test\\{file}.js') as js_file:
+        for line in js_file.readlines():
+            js_input += line
+        js_classname_raw = re.findall("class\s\w{3,}", js_input)
+        js_classnames = []
+        for match in js_classname_raw:
+            classname = re.search("class\s\w{3,}", match)
+            s = classname.start()
+            e = classname.end()
+            classname = classname.string[s:e]
+            classname = classname.split(" ")[1]
+            js_classnames.append(classname)
+
+        js_file__for_split = re.sub("class\s", "filjjndfs789er45jkngdrijouerga890e4jndrclass ", js_input)
+        js_file_split = re.split("filjjndfs789er45jkngdrijouerga890e4jndr", js_file__for_split)
+        js_attributes = {}
+        for i in js_file_split:
+            if i[0] + i[1] + i[2] + i[3] + i[4] != "class":
+                js_file_split.remove(i)
+            else:
+                classname = re.search("class\s\w{3,}", i)
+                s = classname.start()
+                e = classname.end()
+                classname = classname.string[s:e]
+                classname = classname.split(" ")[1]
+                js_attributes[classname] = re.findall("this.\w{1,}", i)
+
+        print(js_file_split)
+        print(js_classnames)
+        print(js_attributes)
+
 
 if __name__ == '__main__':
-    run_nosejs('js_test\\')
-    run_dukpy('js_test\\tripMain.ts')
-    run_js2py('tripList')
+    # run_nosejs('js_test\\')
+    # run_dukpy('js_test\\tripMain.ts')
+    # run_js2py('tripList')
+    run_regex('tripList')
 
 
