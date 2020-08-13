b438a4756870711d7c291507a1c844626fbe9c13
ChironEvans <chiron.evans@gmail.com>
2020-08-14 10:46:58 +1200

    JS Parsing code ported to an object

--- JSParser.py
+++ JSParser.py
@@ -1,32 +1,57 @@
+import os
 import re
+import subprocess
 from re import findall, sub, split
+from os import path, listdir
+
+from graphviz import render
+
 
 class JSParser():
 
-    def __init__(self, target_dir='input\\'):
-        self.target_dir = target_dir
+    def __init__(self, target='input\\'):
+        self.target = target
+        self.js_classnames = []
+        self.js_attributes = {}
+        self.js_assocs = {}
+        self.js_methods = {}
+        self.run_regex()
 
-    def run_regex(file):
+    def run_regex(self):
+        if os.path.isdir(self.target):
+            for file in os.listdir(self.target):
+                if file.endswith('.js'):
+                    self.analyse_file(self.target + file)
+
+        elif os.path.isfile(self.target):
+            if self.target.endswith('.js'):
+                self.analyse_file(self.target)
+
+    def analyse_file(self, file):
         js_input = ''
-        with open(f'js_test\\{file}.js') as js_file:
+        with open(file) as js_file:
             for line in js_file.readlines():
                 js_input += line
+
+            # Remove all comment blocks
+            js_input = re.sub("\/\*(.|\n)*\*\/", '', js_input)
+            js_input = re.sub("#.*", '', js_input)
             js_classname_raw = re.findall("class\s\w{3,}", js_input)
-            js_classnames = []
+
             for match in js_classname_raw:
                 classname = re.search("class\s\w{3,}", match)
                 s = classname.start()
                 e = classname.end()
                 classname = classname.string[s:e]
                 classname = classname.split(" ")[1]
-                js_classnames.append(classname)
+                if classname not in self.js_classnames:
+                    self.js_classnames.append(classname)
 
+            # Add in a large random string so that regex can split by class without removing the keyword
             js_file_for_split = re.sub("class\s", "filjjndfs789er45jkngdrijouerga890e4jndrclass ", js_input)
             js_file_split = re.split("filjjndfs789er45jkngdrijouerga890e4jndr", js_file_for_split)
-            js_attributes = {}
-            js_methods = {}
-            js_assocs = {}
             bad_sectors = []
+
             i = 0
             while i < len(js_file_split):
                 if js_file_split[i][0] + js_file_split[i][1] + js_file_split[i][2] + js_file_split[i][3] + \
@@ -41,39 +66,38 @@ class JSParser():
                     js_attributes_cleaned = set([])
                     for attr in js_attributes_raw:
                         js_attributes_cleaned.add(attr.replace('this.', ''))
-                    js_attributes[classname] = js_attributes_cleaned
+                    self.js_attributes[classname] = js_attributes_cleaned
 
                     js_methods_raw = re.findall("\n\s{2}\w{2,}\s\(.*\)", js_file_split[i])
 
                     js_methods_cleaned = set([])
                     for method in js_methods_raw:
                         js_methods_cleaned.add(method.strip("\n  "))
-                    js_methods[classname] = js_methods_cleaned
+                    self.js_methods[classname] = js_methods_cleaned
 
                     associations_raw = re.findall("new\s\w{3,}\(", js_file_split[i])
                     associations_cleaned = set([])
                     for assoc in associations_raw:
-                        associations_cleaned.add(assoc.reaplace("new ", '').replace("(", ''))
-                    js_assocs[classname] = associations_cleaned
+                        associations_cleaned.add(assoc.replace("new ", '').replace("(", ''))
+                    self.js_assocs[classname] = associations_cleaned
                 else:
                     bad_sectors.append(i)
                 i += 1
             for bad_sector in bad_sectors:
                 js_file_split.remove(js_file_split[bad_sector])
 
-        return_set = (js_classnames, js_attributes, js_assocs, js_methods)
-        return return_set
+        return True
 
-    def write_dotfile(self, classnames, attributes, assocs, methods):
+    def write_dotfile(self):
         with open("output\\classes.dot", "w+") as dot_target:
             dot_target.write('digraph "classes_test" {\ncharset="utf-8"\nrankdir=BT\n')
             class_num = 0
             class_index = {}
-            for js_class in classnames:
-                class_name = classnames[class_num]
+            for js_class in self.js_classnames:
+                class_name = self.js_classnames[class_num]
                 print(class_name)
-                class_attrs = attributes[class_name]
-                class_methods = methods[class_name]
+                class_attrs = self.js_attributes[class_name]
+                class_methods = self.js_methods[class_name]
                 class_index[class_name] = class_num
                 output_string = f'"{class_num}" [label="' + '{' + f'{class_name}|'
 
@@ -90,7 +114,7 @@ class JSParser():
                 class_num += 1
 
             for primary in class_index:
-                class_assocs = assocs[primary]
+                class_assocs = self.js_assocs[primary]
                 for assoc in class_assocs:
                     if assoc in class_index:
                         assoc_index = class_index[assoc]
@@ -98,4 +122,15 @@ class JSParser():
                             f'"{class_index[primary]}" -> "{assoc_index}" [arrowhead="empty", arrowtail="none"];\n')
 
             dot_target.write("}\n")
-            return True
+        self.render_png()
+        return True
+
+    def render_png(self):
+        # Convert a .dot file to .png
+        os.environ["PATH"] += os.pathsep + 'graphviz-2.38-win32/release/bin/'
+        render('dot', 'png', 'output\\classes.dot')
+
+
+if __name__ == '__main__':
+    js_test = JSParser()
+    js_test.write_dotfile()
